8751314647cde10d5ba3184d0b7c799ec9d6fa3b
Serdar AKBAS <xsedo@msn.com>
2022-09-01 16:36:44 +0300

    new

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import RecipeTitle from './RecipeTitle'
+
+function App() {
+    return (
+        <article>
+            <div>{ Date.now() }</div>
+            <h1>Test Yapılıyor</h1>
+            <RecipeTitle />
+        </article>
+    )
+}
+
+export default App;
